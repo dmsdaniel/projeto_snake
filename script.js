64141d1af21d63d8dfb4a5aef0c57258fc08c386
Daniel Santos <dmsdaniel@gmail.com>
2020-09-24 18:57:55 -0300

    criar snake

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+
+snake[0] = {x: 8 * box,
+            y: 8 * box};
+
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0,0,16*box,16*box);
 }
 
 function criaSnake(){
-    context.fillStyle = "black";
-    context.fillRect(8*box, 8*box,32,32);
+    for(i=0; i < snake.length;i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x,snake[i].y,box,box);
+    }
+    
 }
 
 criarBG();
